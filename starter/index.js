8cf8fcf43df69c42a314e76049ab44337f2410a5
Loveday-Alfred <alfredemmanuel2018@yahoo.com>
2022-06-09 07:55:46 +0100

    routing

--- starter/index.js
+++ starter/index.js
@@ -1,8 +1,55 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+////////////////////////////////////////////////////
+// FILES SYSTEM
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File Written!!");
+// Blocking, synchronous way
+
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File Written!!");
+
+// Non-Blocking asynchronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("ERROR!!!");
+
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written");
+//       });
+//     });
+//   });
+// });
+
+// console.log("Will read file");
+
+//////////////////////////////////////////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the Overview page");
+  } else if (pathName === "/product") {
+    res.end("This is the Product page");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-owm-header": "hello-world",
+    });
+    res.end("<h1>page not found!!!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
