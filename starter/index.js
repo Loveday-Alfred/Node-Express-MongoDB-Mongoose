a2a182da378d4c38033b406700e3623edb60a393
Loveday-Alfred <alfredemmanuel2018@yahoo.com>
2022-06-09 22:24:25 +0100

    modules

--- starter/index.js
+++ starter/index.js
@@ -1,12 +1,11 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
-// const slugify = require("slugify");
 
 ////////////////////////////////////////////////////
 // FILES SYSTEM
-
 // Blocking, synchronous way
 
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -52,7 +51,7 @@ const tempProduct = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
-const slugs = dataObj.map((el) => (el.productName, { lower: true }));
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 console.log(slugs);
 
 const server = http.createServer((req, res) => {
