34254320288723181f7655a95e786f6ba46db30d
Loveday-Alfred <alfredemmanuel2018@yahoo.com>
2022-06-12 01:12:06 +0100

    API [GET]

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,14 +1,29 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello From the server', app: 'Natours' });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this end point...');
-});
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello From the server', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this end point...');
+// });
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
