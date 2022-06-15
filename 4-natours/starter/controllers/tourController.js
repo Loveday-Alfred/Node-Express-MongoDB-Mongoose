89006f3a7b7fbda5a8841a899443cd216d2fea12
Loveday-Alfred <alfredemmanuel2018@yahoo.com>
2022-06-15 21:01:26 +0100

    bug [fix]

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -59,7 +59,7 @@ exports.getTour = (req, res) => {
 
 exports.createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, ...req.body);
+  const newTour = Object.assign(req.body, { id: newId });
 
   tours.push(newTour);
 
