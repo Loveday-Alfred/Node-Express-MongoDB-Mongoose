49dbb7419d8fe842820c5cf0861b08438e5f0d94
Loveday-Alfred <alfredemmanuel2018@yahoo.com>
2022-06-21 09:53:42 +0100

    mongoDB & mongoose

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((connect) => {
+    console.log(connect.connections);
+    console.log('Success');
+  });
 
 const app = require('./app');
 
