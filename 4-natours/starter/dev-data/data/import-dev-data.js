51da5d2c80841e5ca7ca50e181cd261ed6cc63dd
Loveday-Alfred <alfredemmanuel2018@yahoo.com>
2022-06-21 22:39:50 +0100

    script to add data to DB

---
+++ 4-natours/starter/dev-data/data/import-dev-data.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const fs = require('fs');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    // console.log(connect.connections);
+    console.log('DB Connected');
+  });
+
+//   Read JSON file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// Import Data into DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data Successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// Delete Data from DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data Successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
