2265823d58f8000312ae0514d34c43914df67d90
Loveday-Alfred <alfredemmanuel2018@yahoo.com>
2022-06-21 15:45:08 +0100

    mongoose model

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
@@ -13,13 +14,30 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then((connect) => {
-    console.log(connect.connections);
-    console.log('Success');
+    // console.log(connect.connections);
+    // console.log('Success');
   });
 
-const app = require('./app');
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 // Starts Server
 const port = process.env.PORT || 3000;
